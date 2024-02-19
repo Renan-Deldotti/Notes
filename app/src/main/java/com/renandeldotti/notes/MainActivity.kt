423269eaa2755e84bea508455fa37917cdfbcae6
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2024-02-19 20:52:41 -0300

    Add basic splash screen

--- app/src/main/java/com/renandeldotti/notes/MainActivity.kt
+++ app/src/main/java/com/renandeldotti/notes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.renandeldotti.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
